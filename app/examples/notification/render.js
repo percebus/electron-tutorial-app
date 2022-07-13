e86f81ac470c5dee56c9936b1a5d1435fe7e4c32
JC Guerrero <JCystems@gMail.com>
2022-07-13 11:04:48 -0500

    feat(notification)

---
+++ app/examples/notification/render.js
@@ -0,0 +1,15 @@
+(() => {
+  'use strict'
+
+  const oNotification = new Notification(
+    'render.js Notification', {
+      body: 'examples/notification/render.js: Click to log to console.'
+    }
+  )
+
+  oNotification.onclick = () => {
+    console.log('notification.click()')
+    const msg = 'Notification clicked!'
+    document.getElementById('output').innerText = msg
+  }
+})()
--- app/render.js
+++ app/render.js
@@ -3,17 +3,36 @@
 
   console.info('render.js: loading...')
 
-  document
-    .getElementById('toggle-dark-mode')
-    .addEventListener('click', async () => {
-      const isDarkMode = await window.darkMode.toggle()
-      document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
-    })
+  function setupDarkColors () {
+    document
+      .getElementById('toggle-dark-mode')
+      .addEventListener('click', async () => {
+        const isDarkMode = await window.darkMode.toggle()
+        document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+      })
 
-  document
-    .getElementById('reset-to-system')
-    .addEventListener('click', async () => {
-      await window.darkMode.system()
-      document.getElementById('theme-source').innerHTML = 'System'
-    })
+    document
+      .getElementById('reset-to-system')
+      .addEventListener('click', async () => {
+        await window.darkMode.system()
+        document.getElementById('theme-source').innerHTML = 'System'
+      })
+  }
+
+  function setupNotification () { // notification
+    const oNotification = new Notification(
+      'Title', {
+        body: 'Notification from the Renderer process. Click to log to console.'
+      }
+    )
+
+    oNotification.onclick = () => {
+      console.log('notification.click()')
+      const msg = 'Notification clicked!'
+      document.getElementById('output').innerText = msg
+    }
+  }
+
+  setupDarkColors()
+  setupNotification()
 })()
